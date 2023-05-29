21ed46e7ecc012e5759a82681574f475416d9c56
CesarJZO <cesarjzo@outlook.com>
2023-05-28 23:55:30 -0600

    It's something...

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
-/**
- * Builds a message based on the name provided
- * @param name The name of the person to greet
- * @returns A greeting message
- */
-function greet(name: string): string {
-    return `Hello ${name}`;
+import * as d3 from 'd3';
+
+const temperatureValue = 25;
+const humidityValue = 50;
+
+function updateTemperatureBar(value: number) {
+    const temperatureBar = d3.select('#temperature-bar');
+
+    temperatureBar
+        .style('width', `${value}%`)
+        .text(`${value}°C`);
 }
 
-/**
- * Contains the message that will be displayed
- */
-let message: string = greet('César');
-console.log(message);
+function updateHumidityGauge(value: number) {
+    const humidityGauge = d3.select('#humidity-gauge');
+
+    humidityGauge
+        .style('transform', `rotate(${value * 1.8 - 90}deg)`)
+        .text(`${value}%`);
+}
+
+updateTemperatureBar(temperatureValue);
+updateHumidityGauge(humidityValue);
