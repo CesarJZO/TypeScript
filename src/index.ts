5296b7127b3d6c9e45b43c41429647538da959c8
CésarJZO <cesarjzo@outlook.com>
2023-05-29 17:41:39 -0600

    Add simple hello world

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,6 @@
-import * as d3 from 'd3';
-
-const temperatureValue = 25;
-const humidityValue = 50;
-
-function updateTemperatureBar(value: number) {
-    const temperatureBar = d3.select('#temperature-bar');
-
-    temperatureBar
-        .style('width', `${value}%`)
-        .text(`${value}°C`);
+function helloWorld(name?: string) {
+    return `Hello ${name || 'World'}!`;
 }
 
-function updateHumidityGauge(value: number) {
-    const humidityGauge = d3.select('#humidity-gauge');
-
-    humidityGauge
-        .style('transform', `rotate(${value * 1.8 - 90}deg)`)
-        .text(`${value}%`);
-}
-
-updateTemperatureBar(temperatureValue);
-updateHumidityGauge(humidityValue);
+console.log(helloWorld());
+console.log(helloWorld('César'));
